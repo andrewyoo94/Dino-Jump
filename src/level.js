06e7f619b51a7742b5dc56882aaa8a82206af24a
Andrew <andrewyoo.ny@gmail.com>
2020-11-12 20:31:01 -0500

    Created first platform

--- src/level.js
+++ src/level.js
@@ -1,10 +1,47 @@
+const CONSTANTS = {
+    PIPE_WIDTH: 75
+};
+
 export default class Level {
     constructor(dimensions) {
         this.dimensions = dimensions;
+
+        // this.platform = [
+        //     this.randomPlatform(firstPlatformDistance)
+            // this.randomPlatform(firstPlatformDistance + CONSTANTS.Platform_SPACING),
+            // this.randomPlatform(firstPlatformDistance + (CONSTANTS.Platform_SPACING * 2)),
+        // ];
+    }
+
+    // randomPlatform(x) {
+    //     const heightRange = this.dimensions.height - (2 * CONSTANTS.EDGE_BUFFER) - CONSTANTS.GAP_HEIGHT;
+    //     const gapTop = (Math.random() * heightRange) + CONSTANTS.EDGE_BUFFER;
+    //     const platform = {
+    //         dimensions: {
+    //             left: x,
+    //             right: CONSTANTS.PIPE_WIDTH + x,
+    //             top: 0,
+    //             bottom: gapTop
+    //         },
+    //         passed: false
+    //     };
+    //     return platform
+    // }
+
+    drawPlatforms(ctx) {
+        ctx.fillStyle = "black";
+
+        ctx.fillRect(
+            50,
+            20,
+            100,
+            10
+        );
     }
 
     animate(ctx) {
         this.drawBackground(ctx);
+        this.drawPlatforms(ctx);
     }
 
     drawBackground(ctx) {
