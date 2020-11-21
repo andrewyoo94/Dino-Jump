4af533cba8dc69a1b8f2cc04a129ec653d803211
Andrew <andrewyoo.ny@gmail.com>
2020-11-20 20:11:13 -0500

    Added assets file

---
+++ src/assets.js
@@ -0,0 +1,10 @@
+let img = new Image();
+img.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
+
+img.onload = function () {
+    init();
+};
+
+function init() {
+    // future animation code goes here
+}
