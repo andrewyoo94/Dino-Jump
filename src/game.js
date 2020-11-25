cf6d450e739a3a15b8ebbf6bbc94ebda15a23769
Andrew <andrewyoo.ny@gmail.com>
2020-11-25 17:09:20 -0500

    Changes to dino movement

--- src/game.js
+++ src/game.js
@@ -1,18 +1,23 @@
 import Level from "./level";
 import Dino from "./dino";
 
+const keys = []
+
 export default class Game {
     constructor(canvas) {
         this.ctx = canvas.getContext("2d");
         this.dimensions = { width: canvas.width, height: canvas.height };
         this.registerEvents();
         this.restart();
+
+        // this.keys = [];
     }
     
     animate() {
         //first we move and draw the level
         this.level.animate(this.ctx);
         this.dino.animate(this.ctx);
+        this.move();
 
         // this.ctx.drawSprite(this.dinoSprite, 0, 0, 50, 50, 0, 0)
 
@@ -40,16 +45,55 @@ export default class Game {
         this.animate();
     }
 
+    // registerEvents() {
+    //     this.boundClickHandler = this.input.bind(this);
+
+    //     document.addEventListener("keydown", this.boundClickHandler {
+    //         this.keys[e.key] = true;
+    //     })
+
+    //     document.addEventListener("keyup", this.boundClickHandler)  {
+    //         delete this.keys[e.key];
+    //     })
+    // }
+
+    // input() {
+    //     if (this.keys[65]) { // A or Arrow_Left
+    //         this.dino.controlDino("left");
+    //     }
+    //     if (this.keys[68]) { // D or Arrow_Right
+    //         this.dino.controlDino("right");
+    //     }
+    // }
+
     registerEvents() {
         this.boundClickHandler = this.input.bind(this);
         document.addEventListener("keydown", this.boundClickHandler);
     }
 
     input(event) {
-        if (event.keyCode === 65 || event.keyCode === 37) { // A or Arrow_Left
+        document.addEventListener("keydown", function(e) {
+            keys[e.key] = true;
+        })
+
+        document.addEventListener("keyup", function(e)  {
+            delete keys[e.key];
+        })
+
+        if (keys[65]) { // A or Arrow_Left
             this.dino.controlDino("left");
         }
-        if (event.keyCode === 68 || event.keyCode === 39) { // D or Arrow_Right
+        if (keys[68]) { // D or Arrow_Right
+            this.dino.controlDino("right");
+        }
+    }
+
+    move() {
+        debugger
+        if (keys[65]) { // A or Arrow_Left
+            this.dino.controlDino("left");
+        }
+        if (keys[68]) { // D or Arrow_Right
             this.dino.controlDino("right");
         }
     }
