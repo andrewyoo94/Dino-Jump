b88959ebb38280d18e1f0aab04c4ffd72d5787d4
Andrew <andrewyoo.ny@gmail.com>
2021-02-10 19:21:25 -0500

    Reformatted info-master

--- src/game.js
+++ src/game.js
@@ -285,7 +285,6 @@ export default class Game {
         
         this.title.animate(this.ctx);
         if (this.running) {
-            debugger
             requestAnimationFrame(this.animate.bind(this));
         }
     } 
